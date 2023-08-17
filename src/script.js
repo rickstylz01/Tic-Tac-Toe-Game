627fd225a0b897244e86143a946c41a190cc880a
Ricardo Maya <rickmaya@gmail.com>
2023-08-17 16:12:37 -0500

    create a function to determine a winner, incomplete

--- src/script.js
+++ src/script.js
@@ -1,20 +1,15 @@
-const tokenX = 'X';
-const tokenO = 'O';
+const tokenX = "X";
+const tokenO = "O";
 let currentToken = tokenX;
-const winConditions = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-  [0, 4, 8], [2, 4, 6]
-];
-playerChoices = []; 
+let playerChoice = [];
 
 // toggle between 'X' and 'O' tokens
 function toggleToken() {
-  currentToken = (currentToken === tokenX) ? tokenO : tokenX;
+  currentToken = currentToken === tokenX ? tokenO : tokenX;
 }
 
 function addToken(block) {
-  //if clicked box has no inner text then set it and toggle token
+  //if clicked block has no inner text then set it and toggle token
   if (!block.innerText) {
     block.innerText = currentToken;
     toggleToken();
@@ -23,21 +18,22 @@ function addToken(block) {
 
 function handleBlockClick(event) {
   const clickedBlock = event.target;
-  const clickedBlockId = parseInt(clickedBlock.id);
-
   addToken(clickedBlock);
-  const blockText = clickedBlock.textContent;
-  playerChoices.push(blockText);
 
-  console.log(playerChoices);
-  console.log('block id: ' + clickedBlockId)
+  playerChoice.push({ id: clickedBlock.id, value: clickedBlock.innerText });
+  console.log(playerChoice)
+
+  determineWinner();
 }
-//iterate over winning condition elements
-// use numbers in the winning condition elements to check the value
-// of the indexes in the player choice array
 
-
-const allBlocks = document.querySelectorAll('.block');
-allBlocks.forEach(block => {
-  block.addEventListener('click',handleBlockClick)
+const allBlocks = document.querySelectorAll(".block");
+allBlocks.forEach((block) => {
+  block.addEventListener("click", handleBlockClick);
 });
+
+//DETERMINE winner
+function determineWinner() {
+  for (let i = 0; i < playerChoice.length; i++) {
+    console.log(playerChoice[i].value);
+  }
+}
