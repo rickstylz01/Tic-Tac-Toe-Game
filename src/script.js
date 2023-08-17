597695992d61789adac394ee11bf8bc6e5977a8f
Ricardo Maya <rickmaya@gmail.com>
2023-08-17 12:36:08 -0500

    obtain the value of the clicked block

--- src/script.js
+++ src/script.js
@@ -1,36 +1,43 @@
-// add event listener
-  // create a method that toggles between x and o when a block is clicked
-// when a square is clicked add an x
-// find a way to toggle from 'x' to 'o'
 const tokenX = 'X';
 const tokenO = 'O';
 let currentToken = tokenX;
+const winConditions = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+  [0, 4, 8], [2, 4, 6]
+];
+playerChoices = []; 
 
 // toggle between 'X' and 'O' tokens
 function toggleToken() {
-  // if currentPlayer equals tokenX then swith to tokenO and vice versa
   currentToken = (currentToken === tokenX) ? tokenO : tokenX;
 }
 
-// if current block has no inner text, set it with the current player token
-// toggle current token
 function addToken(block) {
+  //if clicked box has no inner text then set it and toggle token
   if (!block.innerText) {
     block.innerText = currentToken;
     toggleToken();
   }
 }
 
-// use the addToken function on current block
 function handleBlockClick(event) {
   const clickedBlock = event.target;
+  const clickedBlockId = parseInt(clickedBlock.id);
+
   addToken(clickedBlock);
+  const blockText = clickedBlock.textContent;
+  playerChoices.push(blockText);
+
+  console.log(playerChoices);
+  console.log('block id: ' + clickedBlockId)
 }
+//iterate over winning condition elements
+// use numbers in the winning condition elements to check the value
+// of the indexes in the player choice array
 
 
 const allBlocks = document.querySelectorAll('.block');
-console.log(allBlocks);
 allBlocks.forEach(block => {
-  block.innerText = "";
   block.addEventListener('click',handleBlockClick)
-})
+});
