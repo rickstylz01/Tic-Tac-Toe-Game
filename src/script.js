00e427811bba20ab3f62ceb961c43801c4ac11a4
Ricardo Maya <rickmaya@gmail.com>
2023-08-21 20:58:27 -0500

    clean up code

--- src/script.js
+++ src/script.js
@@ -7,6 +7,10 @@ let currentToken = tokenX;
 let timerHidden = true;
 let setTime = 6;
 
+// score boards
+const player1ScoreBoard = document.querySelector("#score-board-1");
+const player2ScoreBoard = document.querySelector("#score-board-2");
+
 // setting player names
 let player1UsrName = prompt("Player 1, what is your name?");
 let player2UsrName = prompt("Player 2, what is your name?");
@@ -18,14 +22,9 @@ const player2Div = document.querySelector(".player2");
 player1Div.innerText = !player1UsrName ? "Player 1" : player1UsrName;
 player2Div.innerText = !player2UsrName ? "Player 2" : player2UsrName;
 
-// setting initial scores
-const player1ScoreBoard = document.querySelector('#score-board-1');
-const player2ScoreBoard = document.querySelector('#score-board-2');
-
 // toggle between 'X' and 'O' tokens
 function toggleToken() {
   currentToken = currentToken === tokenX ? tokenO : tokenX;
-  setTime = 10;
 }
 
 // if clicked block has no inner text then set it and toggle token
@@ -43,46 +42,50 @@ function handleBlockClick(event) {
     const clickedBlock = event.target;
 
     addToken(clickedBlock);
-    
+
     const winner = checkWinner();
     if (winner) {
       gameOver = true;
-    }
-    
-    if (timerHidden) {
-      timerHidden = false;
-      document.querySelector('#countdown').classList.remove('hidden');
+      clearInterval(timer);
+      document.querySelector('#countdown').classList.add('hidden');
+    } else {
+      // start timer when block is clicked
+      clearInterval(timer);
+      setTime = 6;
+      timer = startTimer();
     }
 
-    // start timere when block is clicked
-    clearInterval(timer);
-    setTime = 6;
-    timer = startTimer();
+    if (timerHidden) {
+      timerHidden = false;
+      document.querySelector("#countdown").classList.remove("hidden");
+    }
   }
 }
 
-//stackoverflow topic on creating a countdown timer(https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown)
+// stackoverflow topic on creating a countdown timer(https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown)
+let timer;
 function startTimer() {
-  return setInterval(function() {
+  return setInterval(function () {
     if (setTime <= 0) {
       clearInterval(timer);
       gameOver = true;
-      document.querySelector('#countdown').innerText = 'Times Up';
+      document.querySelector("#countdown").innerText = "Times Up";
       playerPenalty(currentToken);
       displayLoseModal(currentToken);
-      footerOptions.classList.toggle('hidden');
+      footerOptions.classList.remove("hidden");
     } else {
-      document.querySelector('#countdown').innerText = `${currentToken === tokenX ? player1Div.innerText : player2Div.innerText}'s turn - 00:0${setTime}`;
+      document.querySelector("#countdown").innerText = `${
+        currentToken === tokenX ? player1Div.innerText : player2Div.innerText
+      }'s turn - 00:0${setTime}`;
     }
     setTime -= 1;
   }, 1000);
 }
-let timer;
 
 // get footer element
-const footerOptions = document.querySelector('footer');
+const footerOptions = document.querySelector("footer");
 function toggleGameOptions() {
-  footerOptions.classList.toggle('hidden');
+  footerOptions.classList.toggle("hidden");
 }
 
 let winningPlayer;
@@ -112,12 +115,29 @@ function checkWinner() {
 
     if (theresAWinner) {
       awardWinningPlayer(gameBoard[blockId0].innerText);
-      displayWinModal(winningPlayer);  
+      displayWinModal(winningPlayer);
       toggleGameOptions();
       return winningPlayer;
     }
   }
   findDraw();
+}
+
+function findDraw() {
+  let allMoves = 0;
+  gameBoard.forEach((block) => {
+    if (block.innerText !== "") {
+      allMoves++;
+    }
+  });
+  //if allMoves equals 9 (and a winner was not found)
+  if (allMoves === 9) {
+    clearInterval(timer);
+    gameOver = true;
+
+    displayDrawModal();
+    toggleGameOptions();
+  }
 }
 
 // award winner depending on what the token value is
@@ -136,11 +156,11 @@ function awardWinningPlayer(token) {
 // penalize player by decreasing score count
 function playerPenalty(token) {
   if (token === tokenX) {
-    penaltyPlayer = player1Div.innerText;
+    //penaltyPlayer = player1Div.innerText;
     player1Score--;
     player1ScoreBoard.innerHTML = `Score: ${player1Score}`;
   } else {
-    penaltyPlayer = player2Div.innerText;
+    //penaltyPlayer = player2Div.innerText;
     player2Score--;
     player2ScoreBoard.innerHTML = `Score: ${player2Score}`;
   }
@@ -148,88 +168,76 @@ function playerPenalty(token) {
 
 function displayWinModal(winningPlayer) {
   // set the winner's name in the modal
-  const modalWinnerName = document.getElementById('modalWinnerName');
+  const modalWinnerName = document.getElementById("modalWinnerName");
   modalWinnerName.innerText = `${winningPlayer} Wins!`;
-  
+
   // show the modal
-  const winnerModal = new bootstrap.Modal(document.getElementById('winnerModal'));
-  winnerModal.show();  
+  const winnerModal = new bootstrap.Modal(
+    document.getElementById("winnerModal")
+  );
+  winnerModal.show();
 }
 
 function displayLoseModal(losingPlayer) {
-  clearInterval(timer);
-  gameOver = true;
   // set the loser's name in the modal
-  const modalLoserName = document.getElementById('modalLoserName');
-  modalLoserName.innerText = `${losingPlayer === tokenX ? player1Div.innerText : player2Div.innerText} Lost!`;
-  
+  const modalLoserName = document.getElementById("modalLoserName");
+  modalLoserName.innerText = `${
+    losingPlayer === tokenX ? player1Div.innerText : player2Div.innerText
+  } Lost!`;
+
   // show the modal
-  const losingModal = new bootstrap.Modal(document.getElementById('loserModal'));
-  losingModal.show();  
+  const losingModal = new bootstrap.Modal(
+    document.getElementById("loserModal")
+  );
+  losingModal.show();
 }
 
 function displayDrawModal() {
   // show the modal
-  const drawModal = new bootstrap.Modal(document.getElementById('drawModal'));
-  drawModal.show();  
-}
-
-function findDraw() {
-  let allMoves = 0;
-  gameBoard.forEach((block) => {
-    if (block.innerText !== "") {
-      allMoves++;
-    }
-  });
-  //if allMoves equals 9 (and a winner was not found)
-  if (allMoves === 9) {
-    displayDrawModal();
-    toggleGameOptions()
-    gameOver = true;
-  }
+  const drawModal = new bootstrap.Modal(document.getElementById("drawModal"));
+  drawModal.show();
 }
 
 //gets all the blocks from the gameboard and adds event listener
 const gameBoard = document.querySelectorAll(".block");
 gameBoard.forEach((block) => {
   block.addEventListener("click", handleBlockClick);
-  block.style.cursor = 'pointer';
+  block.style.cursor = "pointer";
 });
 
 // reset function
 const resetBtn = document.querySelector("#reset-btn");
 resetBtn.addEventListener("click", () => {
   // clear the existing timer interval
-  clearInterval(timer);
+  //clearInterval(timer);
 
+  timerHidden = true;
   gameOver = false;
   setTime = 6;
   player1Score = 0;
   player2Score = 0;
-  player1ScoreBoard.innerHTML = 'Score: 0';
-  player2ScoreBoard.innerHTML = 'Score: 0';
+  player1ScoreBoard.innerHTML = "Score: 0";
+  player2ScoreBoard.innerHTML = "Score: 0";
   currentToken = tokenX;
   gameBoard.forEach((block) => (block.innerText = ""));
-  document.querySelector('#countdown').innerText = '';
+  document.querySelector("#countdown").innerText = "";
 
   // prompt for player usernames again
   player1UsrName = prompt("Player 1, what is your name?");
   player2UsrName = prompt("Player 2, what is your name?");
-  
-  // update player names 
+
+  // update player names
   player1Div.innerText = !player1UsrName ? "Player 1" : player1UsrName;
   player2Div.innerText = !player2UsrName ? "Player 2" : player2UsrName;
   toggleGameOptions();
 });
 
-const continueBtn = document.querySelector('#continue-btn');
-continueBtn.addEventListener('click', () => {
-  clearInterval(timer);
+const continueBtn = document.querySelector("#continue-btn");
+continueBtn.addEventListener("click", () => {
   gameOver = false;
+  timerHidden = true;
   setTime = 6;
   currentToken = tokenX;
-  gameBoard.forEach(block => block.innerText = "");
-  document.querySelector('#countdown').innerText = '';
+  gameBoard.forEach((block) => (block.innerText = ""));
   toggleGameOptions();
 });
-
